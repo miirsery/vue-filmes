6b6160172010222eadb672b56afb07ab8de1accf
miirsery <miirsery.main@gmail.com>
2022-10-20 02:52:47 +0700

    add change active link

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="default-layout">
     <div class="default-layout__container">
-      <header>header</header>
-      <div class="d-flex">
+      <header>Movie Dashboard</header>
+      <div class="d-flex jc-between">
         <the-sidebar />
         <router-view />
       </div>
@@ -13,3 +13,17 @@
 <script lang="ts" setup>
 import TheSidebar from '@/components/layouts/DefaultLayout/TheSidebar/TheSidebar.vue'
 </script>
+
+<style lang="scss" scoped>
+header {
+  padding: 40px 0;
+}
+
+.default-layout {
+  &__container {
+    max-width: 1440px;
+    padding: 0 20px;
+    margin: 0 auto;
+  }
+}
+</style>
