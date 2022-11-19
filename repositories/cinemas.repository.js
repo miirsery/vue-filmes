05d438cbfc983b3483dac52b7f8dcfde21cf08d7
miirsery <miirsery.main@gmail.com>
2022-11-20 01:51:07 +0700

    change get all cinemas

--- repositories/cinemas.repository.js
+++ repositories/cinemas.repository.js
@@ -1,7 +1,7 @@
 const db = require('../db')
 
 module.exports = {
-  getAll: async () => await db.query('SELECT id, title, address, lng, lat, phone FROM cinema'),
+  getAll: async () => await db.query('SELECT id, title, address, lng, lat, description, preview, phone FROM cinema'),
   getOne: async (id) => await db.query('SELECT id, title, address, lng, lat, phone FROM cinema WHERE id=$1', [id]),
   createOne: async (street, location) =>
     await db.query('INSERT INTO cinema (street, location) VALUES ($1, $2) RETURNING *', [street, location]),
